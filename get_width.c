dfe8f66740e6ef509cd76d5e5cdc12903d125669
kipkoechke <chepkwonyke1@gmail.com>
2023-04-25 11:16:34 -0700

    printf

---
+++ get_width.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * get_width - function that calculates the width for printing
+ * @format: Format
+ * @i: Parameter.
+ * @list: list of arguments.
+ *
+ * Return: return width.
+ */
+int get_width(const char *format, int *i, va_list list)
+{
+	int curr_i;
+	int width = 0;
+
+	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	{
+		if (isdigit(format[curr_i]))
+		{
+			width *= 10;
+			width += format[curr_i] - '0';
+		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			width = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
+
+	*i = curr_i - 1;
+
+	return (width);
+}
